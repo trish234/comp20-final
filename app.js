41dc05e24f516d0e7b958d1363e1dd701cec533e
trish234 <trisha.cox@gmail.com>
2020-04-28 01:57:14 -0400

    interval for fetch access token

--- app.js
+++ app.js
@@ -8,8 +8,14 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const dotenv = require('dotenv');
 dotenv.config();
+const fetch = require('node-fetch');
 
-//TODO call post request from index.js to test 
+function intervalFunc() {
+  console.log("In interval func");
+  const update = () =>
+  fetch('https://api.petfinder.com/v2/oauth2/token');
+  update();
+}
 
 /* Configure routes here ------------------------------------------- */
 router.get('/', function (req, res) {
@@ -18,6 +24,7 @@ router.get('/', function (req, res) {
 //special route-- we need to POST the data to the HTTP server of the API every 6 minutes
 //                to get the authentication token
 router.post('https://api.petfinder.com/v2/oauth2/token', function(req, res) {
+  console.log("in post here");
   res.send("grant_type=client_credentials&client_id=" + proccess.env.API-KEY + "&client_secret=" + process.env.API-SECRET);
   console.log('Got body:', req.body);
   //console.log('Access token:', req.body.access_token);
@@ -30,4 +37,9 @@ app.use('/public/', express.static('./public')); //show images on the pages
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-  });
+    //setInterval(intervalFunc, 120000); //should run every 2min
+    setInterval(intervalFunc, 900); //testing purposes
+});
+
+
+  
